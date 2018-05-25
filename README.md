39f3530415f0fcb95314f3ed080c397cfa055a32
zhu <zhu@xiaohongshu.com>
2018-05-25 17:02:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+### webpack 4 配置的不同项目
+
+**feature**
+
+* webpack4-vue-cli
+* webpack4-react-cli
+* webpack4-jq-cli
+
+
+--
+时间充裕就会开始更新
+--
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWbpackPlugin = require('html-webpack-plugin')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'assets/js/[name].][hash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              minimize: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true
+            }
+          }],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  devServer: {
+    port: 8088,
+    overlay: {
+      error: true
+    },
+    proxy: {
+      "/api": {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
+    hot: false,// 如果开启 devServer hot HMR会失效
+    clientLogLevel: "none", // cancel console client log
+    open: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWbpackPlugin({
+      title: 'webpack 4',
+      favicon: '',
+      template: 'src/index.html'
+    }),
+    new ExtractTextWebpackPlugin({
+      filename: 'assets/css/main.[hash:8].css'
+    }),
+  ]
+}
+
+module.exports = config
